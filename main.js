ea422174d9f6730dac05d8e145001a26de299bd0
Andre <andreafragnelli@gmail.com>
2024-07-30 08:47:23 +0200

    fix js

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ $(function () {
     easing: "easeOutExpo",
     scrollSpeed: 1100,
     offset: 0,
-    scrollbars: true, // Abilita le barre di scorrimento predefinite
-    setHeights: false, // Non impostare automaticamente le altezze delle sezioni
+    scrollbars: true,
+    setHeights: false,
     overflowScroll: true,
     updateHash: false,
     touchScroll: true,
     before: function () {},
     after: function () {},
     afterResize: function () {
-      $.scrollify.update(); // Aggiorna Scrollify dopo il ridimensionamento
+      $.scrollify.update();
     },
     afterRender: function () {
-      $.scrollify.update(); // Aggiorna Scrollify dopo il rendering
+      $.scrollify.update();
     },
   });
 
@@ -25,8 +25,8 @@ $(function () {
   let tabcontents = document.getElementsByClassName("tab-contents");
   let tabimgs = document.getElementsByClassName("tab-imgs");
 
-  function opentab(tabname) {
-    // Converti HTMLCollection in array e usa forEach
+  // Rendi la funzione opentab una proprietà dell'oggetto window
+  window.opentab = function (tabname) {
     Array.from(tablinks).forEach(function (tablink) {
       tablink.classList.remove("active-link");
     });
@@ -42,7 +42,7 @@ $(function () {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
     document.getElementById(tabname + "-img").classList.add("active-img");
-  }
+  };
 
   window.addEventListener("scroll", function () {
     let scrollPosition = window.scrollY;
@@ -62,22 +62,16 @@ $(function () {
     sidemenu.style.right = "-200px";
   };
 
-  // Function to disable Scrollify on mobile devices
   function disableScrollifyOnMobile() {
-    // Check if the screen width is less than or equal to 1025px (or the size you desire)
     if (window.innerWidth <= 1025) {
-      // Disable Scrollify
       $.scrollify.disable();
     } else {
-      // Enable Scrollify if the screen width is greater than 1025px
       $.scrollify.enable();
     }
   }
 
-  // Call the function when the document is ready
   disableScrollifyOnMobile();
 
-  // Call the function when the window is resized
   $(window).on("resize", function () {
     disableScrollifyOnMobile();
   });
@@ -89,24 +83,21 @@ $(function () {
     const errorMessage = document.getElementById("error-message");
 
     form.addEventListener("submit", function (event) {
-      event.preventDefault(); // Impedisce l'invio del modulo predefinito
+      event.preventDefault();
 
-      // Invia i dati del modulo a EmailJS
       emailjs.sendForm("service_n7p177b", "template_jov794d", this).then(
         function (response) {
           console.log("Email successfully sent!", response);
-          // Aggiungi qui il codice per gestire la visualizzazione di un messaggio di successo all'utente
-          successMessage.style.display = "block"; // Mostra il messaggio di successo
+          successMessage.style.display = "block";
           setTimeout(function () {
             successMessage.style.display = "none";
           }, 5000);
-          errorMessage.style.display = "none"; // Nasconde il messaggio di errore
+          errorMessage.style.display = "none";
         },
         function (error) {
           console.error("Error sending email, Please try again later.", error);
-          // Aggiungi qui il codice per gestire la visualizzazione di un messaggio di errore all'utente
-          successMessage.style.display = "none"; // Nasconde il messaggio di successo
-          errorMessage.style.display = "block"; // Mostra il messaggio di errore
+          successMessage.style.display = "none";
+          errorMessage.style.display = "block";
           setTimeout(function () {
             errorMessage.style.display = "none";
           }, 5000);
